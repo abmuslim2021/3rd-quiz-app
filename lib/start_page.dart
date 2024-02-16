ea6e18b3304c9fbea0020eb2db68e63e0e6e1f7b
unknown <abdullah.muslim2021@gmail.com>
2024-02-16 16:13:54 +0430

    Start Page Created

---
+++ lib/start_page.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class StartPage extends StatelessWidget {
+  const StartPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 300,
+            color: const Color.fromARGB(150, 255, 255, 255),
+          ),
+          const SizedBox(
+            height: 40,
+          ),
+          const Text(
+            'Learn Flutter The Fun Way!',
+            style: TextStyle(
+              fontSize: 20,
+              color: Colors.white,
+            ),
+          ),
+          const SizedBox(
+            height: 25,
+          ),
+          OutlinedButton.icon(
+            onPressed: () {},
+            
+            style: OutlinedButton.styleFrom(
+              
+              side: const BorderSide(
+                width: 1.0,
+                color: Colors.white,
+              ),
+              foregroundColor: Colors.white,
+              textStyle: const TextStyle(
+                fontSize: 20,
+                color: Colors.white,
+              ),
+            ),
+            label: const Text('Start Quiz!'),
+            icon: const Icon(Icons.arrow_circle_right),
+          ),
+        ],
+      ),
+    );
+  }
+}
